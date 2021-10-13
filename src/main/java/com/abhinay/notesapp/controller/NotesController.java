51609844673bd903bef03ab2415f66d05f42c4c9
AbhinayDomala <92484427+AbhinayDomala@users.noreply.github.com>
2021-10-13 19:00:06 -0400

    Added Put Endpoint

--- src/main/java/com/abhinay/notesapp/controller/NotesController.java
+++ src/main/java/com/abhinay/notesapp/controller/NotesController.java
@@ -1,5 +1,7 @@
 package com.abhinay.notesapp.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +31,23 @@ public class NotesController {
 	@DeleteMapping("/note/{id}")
 	public void deleteNoteById(@PathVariable Long id) {
 		notesService.delete(new Notes(id));
+	}
+	
+	@PutMapping("/note")
+	public ResponseEntity<Object> updateNotes(@RequestBody NotesDTO notes) {
+		Optional<Notes> notesFound = notesService.getNoteById(new Notes(notes.getId()));
+		if(notesFound.isPresent()) {
+			notesService.save(new Notes(
+					notes.getId(),
+					notes.getEmail(),
+					notes.getTitle(),
+					notes.getNote(),
+					notes.getCreateTime(),
+					notes.getLastUpdateTime()));
+			return new ResponseEntity<>(HttpStatus.ACCEPTED);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
 	}
 	
 	@PostMapping("/note")
--- src/main/java/com/abhinay/notesapp/service/NotesServiceImpl.java
+++ src/main/java/com/abhinay/notesapp/service/NotesServiceImpl.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.abhinay.notesapp.entity.Notes;
 import com.abhinay.notesapp.repository.NotesRepository;
 
+@Service
 public class NotesServiceImpl implements NotesService{
 
 	@Autowired NotesRepository repository;
